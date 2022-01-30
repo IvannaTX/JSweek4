48bd4ccf485c145127c453744e2761222d4cbd00
Ivanna Contreras <ivanna.aws@gmail.com>
2022-01-29 21:52:45 -0600

    update

--- script.js
+++ script.js
@@ -11,12 +11,21 @@ console.log(average([1.5, 3, 2.5, 1]));
 
 //MEDIUM: Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 
-let nums = [4,5,6,7,0,1,2];
-console.log(nums.indexOf(0));
-console.log(nums.indexOf(3));
+// console.log(nums.indexOf(0));
+// console.log(nums.indexOf(3));
+
+function findIndexOfNum(numToFind){
+    let numberArray = [4,5,6,7,0,1,2];
+    return numberArray.indexOf(numToFind);
+}
+
+console.log("Medium challenge: ", findIndexOfNum(0));
+
+
 
 /*HARD: Create two buttons, one that when clicked turns the background of the body red. 
 The other, when clicked, turns the body's background color white. 
 Each of the background colors should be defined as class styles and when the buttons are 
 pushed the body's classList is updated to include the correct class for the background.  
 Create a single function that takes an input className and updates the body's classList to ONLY include that className*/
+
